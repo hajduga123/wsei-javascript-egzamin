7b3bc78d7f2cb5a36a0ac4a9809abfbfac0a8d89
hajduga123 <48643496+hajduga123@users.noreply.github.com>
2020-01-26 13:37:16 +0100

    Add files via upload

---
+++ wsei-javascript-egzamin-master/js/zadanie06.js
@@ -0,0 +1,22 @@
+////// ZADANIE 6
+
+window.addEventListener('DOMContentLoaded', function(){
+    let list = document.querySelector('ul#shopping-list');
+
+    document.getElementById('button-1').addEventListener('click', function(){
+        // dopisanie nowego produktu do listy
+        let obj = document.createElement('li');
+        obj.textContent = 'Chleb';
+
+        list.appendChild(obj);
+    });
+
+    document.getElementById('button-2').addEventListener('click', function(){
+        list.removeChild(list.lastChild);
+    });
+
+    document.getElementById('button-3').addEventListener('click', function(){
+        let obj = list.children[1].cloneNode(true);
+        list.appendChild(obj);
+    });
+}); 
